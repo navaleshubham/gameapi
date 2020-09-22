c797e6639707119fd73df70a58e35ea83fbe8dbc
Shubham12051999 <shubham.navale12@gmail.com>
2020-09-22 18:01:40 +0530

    make it better

--- index.js
+++ index.js
@@ -39,7 +39,22 @@ function checkresult(mat){
     var case5y=0
     var case6r=0
     var case6y=0
-
+    for(var j=5;j>=0;j--){    
+        if(mat[j][5-j]==1){
+            case5r=case5r+mat[j][5-j]
+            if(case5r==4){
+                return ('red wins')
+            }
+        }
+        else if(mat[j][5-j]==-1){
+            case5y=case5y+mat[j][5-j]
+            if(case5y==-4){
+        console.log('case5',case5r,case5y)
+                return ('yellow wins')
+            }
+        }
+        console.log('case5',case5r,case5y)
+    }
     for(var j=0;j<6;j++){
         for(var k=0;k<7;k++){
             if(mat[j][k]==1){
@@ -48,13 +63,14 @@ function checkresult(mat){
                     return ('red wins')
                 }
             }
-            else{
+            else if(mat[j][k]==-1){
                 case2y=case2y+mat[j][k]
                 if(case2y==-4){
                     return ('yellow wins')
                 }
             }
         }
+        console.log('case2',case2r,case2y)
         case2r=0
         case2y=0
     }
@@ -66,13 +82,14 @@ function checkresult(mat){
                     return ('red wins')
                 }
             }
-            else{
+            else if(mat[k][j]==-1){
                 case3y=case3y+mat[k][j]
                 if(case3y==-4){
                     return ('yellow wins')
                 }
             }
         }
+        console.log('case3',case3r,case3y)
         case3r=0
         case3y=0
     }
@@ -85,7 +102,7 @@ function checkresult(mat){
                         return ('red wins')
                     }
                 }
-                else{
+                else if(mat[k][j]==-1){
                     case1y=case1y+mat[k][j]
                     if(case1y==-4){
                         return ('yellow wins')
@@ -93,8 +110,10 @@ function checkresult(mat){
                 }
             }
         }
+        console.log('case1',case1r,case1y)
+        
     }
-    for(var j=6;j<=0;j--){
+    for(var j=6;j>=0;j--){
         if(j==0){break;}
         if(mat[6-j][j]==1){
             case4r=case4r+mat[6-j][j]
@@ -102,43 +121,32 @@ function checkresult(mat){
                 return ('red wins')
             }
         }
-        else{
+        else if(mat[6-j][j]==-1){
             case4y=case4y+mat[6-j][j]
             if(case4y==-4){
                 return ('yellow wins')
             }
         }
+        console.log('case4',case4r,case4y)
     }
-    for(var j=5;j<=0;j--){    
-        if(mat[j][5-j]==1){
-            case5r=case5r+mat[j][5-j]
-            if(case5r==4){
-                return ('red wins')
-            }
-        }
-        else{
-            case5y=case5y+mat[j][5-j]
-            if(case5y==-4){
-                return ('yellow wins')
-            }
-        }
-    }
-    for(var j=5;j<=0;j--){
-        for(k=6;k<=0;k--){
-        if(mat[j][k]==1){
-            case6r=case6r+mat[j][k]
-            if(case6r==4){
-                return ('red wins')
-            }
-        }
-        else{
-            case6y=case6y+mat[j][k]
-            if(case6y==-4){
-                return ('yellow wins')
-            }
-        }
-    }
-    }
+    
+    // for(var j=5;j>=0;j--){
+    //     for(k=6;k>=0;k--){
+    //     if(mat[j][k]==1){
+    //         case6r=case6r+mat[j][k]
+    //         if(case6r==4){
+    //             return ('red wins')
+    //         }
+    //     }
+    //     else if(mat[j][k]==-1){
+    //         case6y=case6y+mat[j][k]
+    //         if(case6y==-4){
+    //             console.log('case6',case6r,case6y)
+    //             return ('yellow wins')
+    //         }
+    //     }
+    // }
+    // }
     
 }
 
@@ -191,7 +199,7 @@ app.get('/getentry/:colnum',(req,res)=>{
                     var p={ymoves:colnum+1}
                 }
                 cplay=!cplay
-                console.log(p)
+                // console.log(p)
                 game.updateOne({palyertoken:token},{$set:{Matrix:mat,cplayer:cplay},$push:p},(err,result)=>{
                     if(err) return res.send(403)
                     c=checkresult(mat)
